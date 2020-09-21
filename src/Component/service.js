50047b84f9599f868132f7fd67eb4ffdb7459711
hieu.tran <hieu.tran@bnksolution.com>
2020-09-21 21:51:02 +0700

    react table

--- src/Component/service.js
+++ src/Component/service.js
@@ -1,11 +1,110 @@
 import React from 'react';
+import Table from '../CommonComponent/Table/table';
+import styled from 'styled-components'
+import makeData from './makeData'
 
 function Service() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const [data, setData] = React.useState(() => makeData(40))
+  const [originalData] = React.useState(data)
+  const [skipPageReset, setSkipPageReset] = React.useState(false)
+  const updateMyData = (rowIndex, columnId, value) => {
+    // We also turn on the flag to not reset the page
+    setSkipPageReset(true)
+    setData(old =>
+      old.map((row, index) => {
+        if (index === rowIndex) {
+          return {
+            ...old[rowIndex],
+            [columnId]: value,
+          }
+        }
+        return row
+      })
+    )
+  }
+  React.useEffect(() => {
+    setSkipPageReset(false)
+  }, [data])
+  const resetData = () => setData(originalData)
   return (
-    <div >
-        <p>Service</p>
-    </div>
+    <Styles >
+      <button onClick={resetData}>Reset Data</button>
+          <Table 
+           columns={columns}
+           data={data}
+           updateMyData={updateMyData}
+           skipPageReset={skipPageReset}
+        />
+    </Styles>
   );
 }
 
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
 export default Service;
